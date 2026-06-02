158ed9be1f90a956aff2e0a18d421fbbd84dcb6e
test01-Nguyen Thi Cam Tu <ntcamtuik19lqd@gmail.com>
2026-06-02 20:09:15 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,10 @@
+const playerName = "Mario"
+const currentLives = 3
+const level1 = 25
+const level2 = 30
+const level3 = 45
+const totalCoins = level1 + level2 + level3
+const averageCoins = totalCoins/3
+const remainCoins = totalCoins%3
+console.log(`Player name is: ${playerName}`)
+console.log(`Remainder  coins is: ${remainCoins}`)
